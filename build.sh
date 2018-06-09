142a942340bf6541e44863cb6ede3003981ddb01
Auston Stewart <auston@shedbuilt.net>
2018-06-09 11:54:12 -1000

    Initial commit of chocolate-doom

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Configure
+./configure --prefix=/usr \
+            --with-libpng \
+            --without-libsamplerate &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install
